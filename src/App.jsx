6f61585cb4306b780d3b7cb9c121e45de9c3e995
fatma mhadhbi <fatmaezzahra.mhadhbi@gmail.com>
2026-02-26 17:22:57 +0100

    Week 2: First React component with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+const courseTitle = "React Basics"
 function App() {
-  const [count, setCount] = useState(0)
-
+  const studentName = "Fatma"
+  const student = { name: "Fatma", age: 20, track: "Web Development" }
+  function sayHello() {
+    return `Hello, ${studentName}! Welcome to ${courseTitle}.`
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Hello, my name is Toma!</h1>
+      <p>My name is {studentName}</p>
+      <p>Welcome to {courseTitle}, {studentName}!</p>
+      <label htmlFor="studentInpur">Enter your name</label>
+      <input type="text" id="studentInput"/>
+      <p>Student Name: {student.name}</p>
+      <p>Age: {student.age}</p>
+      <p>Track: {student.track}</p>
+      <p>{sayHello()}</p>
+    </div>
   )
 }
 
